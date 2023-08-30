4476b3be31b0c248a0f7d7c2e515588693a67f5d
LeilaSedigh <leilasedighbakhshaish@gmail.com>
2023-08-30 21:56:37 +0330

    message

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import CartIcon from "./components/Cart/Cart";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const showCartHandler = () => {
-    setCartIsShown(true)
-  }
+  const showCartHandler = () => setCartIsShown(true)
+  
 
-  const hideCartHandler = () => {
-    setCartIsShown(false)
-  }
+  const hideCartHandler = () => setCartIsShown(false)
+  
 
   return (
     <CartProvider>
